6acdb5bc910cb3a1270dc7b6bd95df1de18d90a7
Hailey Somerville <hailey@hailey.lol>
2023-08-23 13:34:10 +1000

    move rate adjuster out into its own mod

--- src/receive/mod.rs
+++ src/receive/mod.rs
@@ -1,16 +1,21 @@
+mod buffer;
+mod rate;
+mod output;
+
 use std::array;
 use std::collections::VecDeque;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 use bytemuck::Zeroable;
-use cpal::{SampleRate, OutputCallbackInfo};
+use cpal::OutputCallbackInfo;
 use cpal::traits::{HostTrait, DeviceTrait};
 use structopt::StructOpt;
 
 use crate::protocol::{self, Protocol};
 use crate::protocol::packet::{Audio, Time, PacketKind, StatsReply};
 use crate::protocol::types::{TimestampMicros, SessionId, ReceiverId, TimePhase};
+use crate::receive::rate::RateAdjust;
 use crate::resample::Resampler;
 use crate::socket::{Socket, SocketOpt};
 use crate::stats::node::NodeStats;
@@ -46,7 +51,7 @@ struct Session {
     start_seq: u64,
     sync: bool,
     resampler: Resampler,
-    rate_adjust: RateAdjust,
+    rate_adjust: rate::RateAdjust,
     latency: Aggregate<Duration>,
     clock_delta: Aggregate<ClockDelta>,
 }
@@ -347,7 +352,7 @@ impl Receiver {
         }
 
         if let Some(stream_ts) = stream_ts {
-            let rate = stream.rate_adjust.sample_rate(Timing {
+            let rate = stream.rate_adjust.sample_rate(rate::Timing {
                 real: real_ts_after_fill,
                 play: stream_ts,
             });
@@ -366,69 +371,6 @@ impl Receiver {
         self.stats.set_buffer_length(self.queue.iter()
             .map(|entry| SampleDuration::ONE_PACKET.sub(entry.consumed))
             .fold(SampleDuration::zero(), |cum, dur| cum.add(dur)));
-    }
-}
-
-struct RateAdjust {
-    slew: bool,
-}
-
-#[derive(Copy, Clone)]
-pub struct Timing {
-    pub real: Timestamp,
-    pub play: Timestamp,
-}
-
-impl RateAdjust {
-    pub fn new() -> Self {
-        RateAdjust {
-            slew: false
-        }
-    }
-
-    pub fn slew(&self) -> bool {
-        self.slew
-    }
-
-    pub fn sample_rate(&mut self, timing: Timing) -> SampleRate {
-        self.adjusted_rate(timing).unwrap_or(protocol::SAMPLE_RATE)
-    }
-
-    fn adjusted_rate(&mut self, timing: Timing) -> Option<SampleRate> {
-        // parameters, maybe these could be cli args?
-        let start_slew_threshold = Duration::from_micros(2000);
-        let stop_slew_threshold = Duration::from_micros(100);
-        let slew_target_duration = Duration::from_millis(500);
-
-        // turn them into native units
-        let start_slew_threshold = SampleDuration::from_std_duration_lossy(start_slew_threshold);
-        let stop_slew_threshold = SampleDuration::from_std_duration_lossy(stop_slew_threshold);
-
-        let frame_offset = timing.real.delta(timing.play);
-
-        if frame_offset.abs() < stop_slew_threshold {
-            self.slew = false;
-            return None;
-        }
-
-        if frame_offset.abs() < start_slew_threshold && !self.slew {
-            return None;
-        }
-
-        let slew_duration_duration = i64::try_from(slew_target_duration.as_micros()).unwrap();
-        let base_sample_rate = i64::from(protocol::SAMPLE_RATE.0);
-        let rate_offset = frame_offset.as_frames() * 1_000_000 / slew_duration_duration;
-        let rate = base_sample_rate + rate_offset;
-
-        // clamp any potential slow down to 2%, we shouldn't ever get too far
-        // ahead of the stream
-        let rate = std::cmp::max(base_sample_rate * 98 / 100, rate);
-
-        // let the speed up run much higher, but keep it reasonable still
-        let rate = std::cmp::min(base_sample_rate * 2, rate);
-
-        self.slew = true;
-        Some(SampleRate(u32::try_from(rate).unwrap()))
     }
 }
 
